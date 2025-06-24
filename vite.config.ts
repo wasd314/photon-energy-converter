78c17ab2879ecb28778642a62f7f61881703a79b
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-06-25 00:08:06 +0900

    PWA support: setup manifest, html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{css,html,ico,js,png,ttf,webmanifest,woff,woff2}'],
+      },
+      manifest: {
+        name: 'Photon Energy Converter',
+        short_name: 'UnitConv',
+        description: 'photon energy unit converter',
+        theme_color: '#eeeeee',
+        background_color: '#111111',
+        orientation: 'any',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
