a56dff39d694e3debb4632cbd6590f6dd81894bd
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-07-01 20:56:40 +0900

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ConversionTable from './ConversionTable';
+
+const theme = createTheme({
+  cssVariables: true,
+  colorSchemes: {
+    light: true,
+    dark: true,
+  },
+});
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <h2>Photon energy converter</h2>
       <ConversionTable />
-    </>
+    </ThemeProvider>
   );
 };
 
