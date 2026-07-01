6a31ea3e152b5a7f93fb20d52db8156c28c36e31
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-07-02 08:56:16 +0900

    add unit, consts

---
+++ src/units/Constant.ts
@@ -0,0 +1,20 @@
+/** c / m s^{-1} */
+export const SPEED_OF_LIGHT = 299792458;
+
+/** h / J s */
+export const PLANCK_CONSTANT = 6.62607015e-34;
+
+/** hbar / J s rad^{-1} */
+export const DIRAC_CONSTANT = PLANCK_CONSTANT / (2 * Math.PI);
+
+/** k_B / J K^{-1} */
+export const BOLTZMANN_CONSTANT = 1.380649e-23;
+
+/** e / C */
+export const ELEMENTARY_CHARGE = 1.602176634e-19;
+
+/** N_A / mol^{-1} */
+export const AVOGADRO_CONSTANT = 6.02214076e23;
+
+/** µ_B / J T^{-1} (not a defining constant; 2022 CODATA recommended values) */
+export const BOHR_MAGNETON = 9.2740100657e-24;
---
+++ src/units/Unit.ts
@@ -0,0 +1,193 @@
+import {
+  AVOGADRO_CONSTANT,
+  BOHR_MAGNETON,
+  BOLTZMANN_CONSTANT,
+  DIRAC_CONSTANT,
+  ELEMENTARY_CHARGE,
+  PLANCK_CONSTANT,
+  SPEED_OF_LIGHT,
+} from './Constant';
+
+interface Unit {
+  /** KaTeX 表示の単位 */
+  mathUnit: string;
+  /** E / J への変換 */
+  toJoule: (x: number) => number;
+  /** E / J からの変換 */
+  fromJoule: (x: number) => number;
+}
+
+interface Quantity {
+  /** 物理量の名前 */
+  quantityName: string;
+  /** KaTeX 表示の量記号 */
+  mathQuantity: string;
+  /** KaTeX 表示のエネルギーとの変換公式 */
+  mathConversionFormula: string;
+  units: Unit[];
+}
+
+/** (E / J) = (x / Unit) * coefficient */
+const proportionalHelper = (coefficient: number) => ({
+  toJoule: (x: number) => x * coefficient,
+  fromJoule: (x: number) => x / coefficient,
+});
+/** (E / J) = coefficient / (x / Unit) */
+const inverseProportionalHelper = (coefficient: number) => ({
+  toJoule: (x: number) => coefficient / x,
+  fromJoule: (x: number) => coefficient / x,
+});
+
+export const quantities: Quantity[] = [
+  {
+    quantityName: 'Energy',
+    mathQuantity: 'E',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{E}{\\mathrm{eV}} \\cdot \\frac{e}{\\mathrm{C}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{J}',
+        // J / J
+        ...proportionalHelper(1),
+      },
+      {
+        mathUnit: '\\mathrm{eV}',
+        // e / C
+        // = e / J V^-1
+        ...proportionalHelper(ELEMENTARY_CHARGE),
+      },
+    ],
+  },
+  {
+    quantityName: 'Molar energy',
+    mathQuantity: 'E_\\text{m}',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{E_\\text{m}}{\\mathrm{kJ} \\, \\mathrm{mol}^{-1}} \\cdot \\frac{N_\\text{A}^{-1}}{\\mathrm{mol}} \\cdot \\frac{\\mathrm{kJ}}{\\mathrm{J}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{kJ} \\, \\mathrm{mol}^{-1}',
+        // N_A^-1 / ((J/kJ) mol)
+        // = (kJ/J) / (N_A mol)
+        ...proportionalHelper(1000 / AVOGADRO_CONSTANT),
+      },
+      {
+        mathUnit: '\\mathrm{kcal} \\, \\mathrm{mol}^{-1}',
+        // N_A^-1 / ((J/kcal) mol)
+        // = (kcal/J) / (N_A mol)
+        ...proportionalHelper(4184 / AVOGADRO_CONSTANT),
+      },
+    ],
+  },
+  {
+    quantityName: 'Wavelength',
+    mathQuantity: '\\lambda',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{\\lambda^{-1}}{\\mathrm{m}^{-1}} \\cdot \\frac{h c}{\\mathrm{J} \\, \\mathrm{m}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{m}',
+        // h c / J m
+        ...inverseProportionalHelper(PLANCK_CONSTANT * SPEED_OF_LIGHT),
+      },
+      {
+        mathUnit: '\\mathrm{nm}',
+        // h c / J nm
+        ...inverseProportionalHelper(PLANCK_CONSTANT * SPEED_OF_LIGHT * 1e9),
+      },
+    ],
+  },
+  {
+    quantityName: 'Wavenumber',
+    mathQuantity: '\\tilde{\\nu}',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{\\tilde{\\nu}}{\\mathrm{cm}^{-1}} \\cdot \\frac{h c}{\\mathrm{J} \\, \\mathrm{cm}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{cm}^{-1}',
+        // h c / J cm
+        ...proportionalHelper(PLANCK_CONSTANT * SPEED_OF_LIGHT * 1e2),
+      },
+    ],
+  },
+  {
+    quantityName: 'Period',
+    mathQuantity: '\\tau',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{\\tau^{-1}}{\\mathrm{s}^{-1}} \\cdot \\frac{h}{\\mathrm{J} \\, \\mathrm{s}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{s}',
+        // h / J s
+        ...inverseProportionalHelper(PLANCK_CONSTANT),
+      },
+      {
+        mathUnit: '\\mathrm{fs}',
+        // h / J fs
+        ...inverseProportionalHelper(PLANCK_CONSTANT),
+      },
+    ],
+  },
+  {
+    quantityName: 'Frequency',
+    mathQuantity: '\\nu',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{\\nu}{\\mathrm{Hz}} \\cdot \\frac{h}{\\mathrm{J} \\, \\mathrm{s}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{Hz}',
+        // h / J s
+        ...proportionalHelper(PLANCK_CONSTANT),
+      },
+      {
+        mathUnit: '\\mathrm{THz}',
+        // h / J THz^-1
+        // = h / J ps
+        ...proportionalHelper(PLANCK_CONSTANT * 1e12),
+      },
+    ],
+  },
+  {
+    quantityName: 'Angular frequency',
+    mathQuantity: '\\omega',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{\\omega}{\\mathrm{rad} \\, \\mathrm{s}^{-1}} \\cdot \\frac{\\hbar}{\\mathrm{J} \\, \\mathrm{s} \\, \\mathrm{rad}^{-1}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{rad} \\, \\mathrm{s}^{-1}',
+        // hbar / J s rad^-1
+        ...proportionalHelper(DIRAC_CONSTANT),
+      },
+      {
+        mathUnit: '\\mathrm{rad} \\, \\mathrm{fs}^{-1}',
+        // hbar / J fs rad^-1
+        ...proportionalHelper(DIRAC_CONSTANT * 1e15),
+      },
+    ],
+  },
+  {
+    quantityName: 'Temperature',
+    mathQuantity: 'T',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{T}{\\mathrm{K}} \\cdot \\frac{k_\\text{B}}{\\mathrm{J} \\, \\mathrm{K}^{-1}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{K}',
+        // k_B / J K^-1
+        ...proportionalHelper(BOLTZMANN_CONSTANT),
+      },
+    ],
+  },
+  {
+    quantityName: 'Magnetic flux density',
+    mathQuantity: 'B',
+    mathConversionFormula:
+      '\\frac{E}{\\mathrm{J}} = \\frac{B}{\\mathrm{T}} \\cdot \\frac{\\mu_\\text{B}}{\\mathrm{J} \\, \\mathrm{T}^{-1}}',
+    units: [
+      {
+        mathUnit: '\\mathrm{T}',
+        // µ_B / J T^-1
+        ...proportionalHelper(BOHR_MAGNETON),
+      },
+    ],
+  },
+];
